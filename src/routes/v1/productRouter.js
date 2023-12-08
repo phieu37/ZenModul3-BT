6996963ae7a985ebb4b622fba13180acfd9a9eaf
phieu37 <86352994+phieu37@users.noreply.github.com>
2023-12-08 09:22:25 +0700

    #1

--- src/routes/v1/productRouter.js
+++ src/routes/v1/productRouter.js
@@ -8,38 +8,42 @@ let products = [
 ];
 
 // Router GET: Thể hiện việc gửi và nhận data qua param, query string
-router.get('/products/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const queryParam = req.query.param;
 
   const product = products.find((product) => product.id === productId);
+  console.log(product);
   res.json({ product, queryParam });
 });
 
 // Router POST: Thể hiện việc gửi và nhận data qua body
-router.post('/products', (req, res) => {
+router.post('/', (req, res) => {
   const newProduct = req.body;
 
   products.push(newProduct);
+  console.log(products);
   res.json({ products });
 });
 
 // Router PUT: Thể hiện việc gửi và nhận data qua body
-router.put('/products/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const updatedProductData = req.body;
 
   products = products.map((product) =>
     product.id === productId ? { ...product, ...updatedProductData } : product
   );
+  console.log(products);
   res.json({ products });
 });
 
 // Router DELETE: Thể hiện việc gửi và nhận data qua param
-router.delete('/products/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const productId = parseInt(req.params.id);
 
   products = products.filter((product) => product.id !== productId);
+  console.log(products);
   res.json({ products });
 });
 
